067418605e97749564a3c9a16ecb7185e32ea00a
quyvuacn <87663363+quyvuacn@users.noreply.github.com>
2021-12-31 09:55:21 +0700

    Quý

--- Ass/ASS03.sql
+++ Ass/ASS03.sql
@@ -31,5 +31,25 @@ VALUES
 (   N'Vũ Viết Quý', -- TenTB - nvarchar(100)
     '034203004985',  -- SCMT - char(20)
     N'Thái Thụy,Thái Bình'  -- Diachi - nvarchar(200)
+    ),
+(   N'Đặng Kim Thi', -- TenTB - nvarchar(100)
+    '123456789',  -- SCMT - char(20)
+    N'Hà Nội'  -- Diachi - nvarchar(200)
+    ),
+(   N'Nguyễn Nguyệt Nga', -- TenTB - nvarchar(100)
+    '1234567890',  -- SCMT - char(20)
+    N'Hà Nội'  -- Diachi - nvarchar(200)
     )
 
+INSERT INTO dbo.SoThueBao
+(   MaTB,
+	SoTB,
+	LoaiTB,
+	NgayDK
+)
+VALUES
+(  	3,        -- MaTB - int
+	'0326459773',       -- SoTB - char(15)
+	0,        -- LoaiTB - int
+	GETDATE() -- NgayDK - date
+	)
