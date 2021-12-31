8e634d728b169ea53a9d233ec6bf7eb0561d2572
quyvuacn <87663363+quyvuacn@users.noreply.github.com>
2021-12-31 10:51:27 +0700

    quý

--- Ass/ASS03.sql
+++ Ass/ASS03.sql
@@ -53,3 +53,63 @@ VALUES
 	0,        -- LoaiTB - int
 	GETDATE() -- NgayDK - date
 	)
+--4: Hiển thị thông tin khách hàng và thông tin thuê bao
+SELECT * FROM dbo.ThueBao
+JOIN dbo.SoThueBao 
+ON SoThueBao.MaTB = ThueBao.MaTB 
+--5a. Hiển thị thông tin thuê bao có CMT là 1234567890
+SELECT * FROM dbo.ThueBao
+WHERE SCMT = '1234567890'
+
+--5b. Hiển thị thông tin thuê bao có Số Thuê bao là 0326459773
+SELECT * FROM dbo.ThueBao
+JOIN dbo.SoThueBao 
+ON SoThueBao.MaTB = ThueBao.MaTB 
+WHERE SoTB = '0326459773'
+
+--5c.Hiển thị các số thuê bao của khách hàng có cmt là 1234567890
+SELECT * FROM dbo.ThueBao
+JOIN dbo.SoThueBao 
+ON SoThueBao.MaTB = ThueBao.MaTB 
+WHERE SCMT = '1234567890'
+
+--5d. Liệt kê các thuê bao đk vào ngày 31/12/2021
+SELECT TenTB FROM dbo.ThueBao
+JOIN dbo.SoThueBao 
+ON SoThueBao.MaTB = ThueBao.MaTB 
+WHERE NgayDK = '20211231'
+
+--5e. Liệt kê thuê bao sống ở Hà Nội
+SELECT * FROM dbo.ThueBao
+WHERE Diachi LIKE N'%Hà Nội%'
+
+--6a. Tổng số khách hàng của công ty
+SELECT COUNT(MaTB) AS 'Khách hàng' FROM dbo.ThueBao
+
+--6b. Tổng số thuê bao của công ty
+SELECT COUNT(MaHopDong) AS 'Hợp đồng' FROM dbo.SoThueBao
+
+--6c. Tổng số thuê bao của công ty đk 12/31/2021
+SELECT COUNT(MaHopDong) AS 'Số hợp đồng' FROM dbo.SoThueBao
+WHERE NgayDK = '20211231'
+
+--6d. Hiển thị toàn bộ thông tin về khách hàng của các số thuê bao
+SELECT * FROM dbo.ThueBao
+JOIN dbo.SoThueBao 
+ON SoThueBao.MaTB = ThueBao.MaTB 
+
+--7ab.Thay đổi ngày đk => not null
+ALTER TABLE dbo.SoThueBao
+	ALTER COLUMN NgayDK DATE NOT NULL
+ALTER TABLE dbo.SoThueBao
+	ADD CHECK(NgayDK<=GETDATE())
+
+--7c. Thay đổi đầu SDT bằng 03 hoặc 09
+ALTER TABLE dbo.SoThueBao
+	ADD CHECK(LEFT(SoTB,2)='03' OR LEFT(SoTB,2)='09')
+
+	
+	
+	
+
+--
