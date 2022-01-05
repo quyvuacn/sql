5ed3aa6d07360b008eaee39f97e760fd5a390920
quyvuacn <87663363+quyvuacn@users.noreply.github.com>
2022-01-05 10:52:12 +0700

    Quý

--- Day_8/Lab08.sql
+++ Day_8/Lab08.sql
@@ -9,4 +9,15 @@ SELECT * INTO Lab08.dbo.WorkOrder FROM Production.WorkOrder
 USE Lab08
 GO
 
-SELECT * INTO WorkOrderIX FROM Production.WorkOrder
+
+SELECT * INTO WorkOrderIX FROM WorkOrder
+
+SELECT*FROM WorkOrder
+SELECT*FROM WorkOrderIX
+
+CREATE INDEX IX_WorkOrderID ON WorkOrderIX (WorkOrderID)
+
+SELECT * FROM WorkOrder WHERE WorkOrderID=72000
+SELECT * FROM WorkOrderIX WHERE WorkOrderID=72000
+
+
