d871690a1eda218d29e9021e3671875011d0a560
quyvuacn <87663363+quyvuacn@users.noreply.github.com>
2022-01-09 22:29:42 +0700

    Quý

---
+++ Task_09-Day_09/StoreProcedure-Insert.sql
@@ -0,0 +1,34 @@
+﻿CREATE DATABASE  Store_Procedure_Insert
+GO
+
+CREATE TABLE NhanVien(
+	MaNV INT IDENTITY PRIMARY KEY,
+	TenNV NVARCHAR(100) NOT NULL,
+	NgaySinh DATE 
+)
+GO
+
+
+CREATE PROCEDURE sp_insert_NhanVien
+	@TenNV NVARCHAR(100),
+	@NgaySinh DATE
+AS 
+BEGIN
+	IF(@TenNV IS NOT NULL )
+	INSERT INTO dbo.NhanVien
+	(
+	    TenNV,
+	    NgaySinh
+	)
+	VALUES
+	(   @TenNV,      -- TenNV - nvarchar(100)
+	    @NgaySinh -- NgaySinh - date
+	    )
+END
+GO
+
+DECLARE @err NVARCHAR(100);
+EXEC dbo.sp_insert_NhanVien @TenNV = N'Vũ Viết Quý',             -- nvarchar(100)
+                            @NgaySinh = '2022-01-09' -- date
+              
+SELECT * FROM dbo.NhanVien
---
+++ Task_09-Day_09/StoreProcedure-Learn.sql
@@ -0,0 +1,54 @@
+﻿USE AdventureWorks2019
+GO
+
+--Tạo thủ tục lưu trữ tìm kiếm tên nước
+CREATE PROCEDURE sp_search
+	@nuoc VARCHAR(20)
+AS
+SELECT * FROM Person.CountryRegion
+WHERE Name = @nuoc
+GO
+
+EXECUTE dbo.sp_search @nuoc = 'Zambia' -- varchar(20)
+GO
+
+-- Trình tự
+-- Tạo
+	--CREATE PROCEDURE [Tên PROCEDURE]
+	-- [@thamso1 type, @thamso2 type,...]
+	--AS
+	--SELECT * FROM [Bảng]
+	--WHERE ĐK ứng với tham số
+--Sử dụng
+	--EXECUTE [tên PROCEDURE] [gtthamso1,gtthamso2,...]
+
+--Đếm xem có bao nhiêu sản phẩm có màu sắc được truyền vào
+--C1
+CREATE PROCEDURE sp_color
+	@color VARCHAR(20),
+	@return INT OUTPUT
+AS
+SELECT @return = COUNT(Color)  FROM Production.Product
+WHERE Color = @color
+GO
+
+DECLARE @return INT;
+EXECUTE dbo.sp_color @color = 'Black',             
+                     @return = @return OUTPUT 
+PRINT @return
+GO
+
+--Đếm xem có bao nhiêu sản phẩm có màu sắc được truyền vào
+--C2
+CREATE PROCEDURE sp_color2
+	@color VARCHAR(20)
+AS
+DECLARE @return INT
+SELECT @return=COUNT(Color) FROM Production.Product
+WHERE Color = @color
+RETURN @return
+GO
+
+DECLARE @return INT
+EXECUTE @return = dbo.sp_color2 @color = 'Black' -- varchar(20)
+PRINT @return
